1a377e42a77a5343c0b68343ae44d06ac65b12ee
MikeBowers <Mike.Bow@live.com>
2018-05-31 18:09:05 -0400

    Initial Deployment
    Initial Deployment

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1527804483,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ball-sheet0.png",
+		"images/rightbarrier-sheet0.png",
+		"images/leftbarrier-sheet0.png",
+		"images/leftside-sheet0.png",
+		"images/block1-sheet0.png",
+		"images/bulletlife-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/gold-sheet0.png",
+		"images/bad-sheet0.png",
+		"images/playerbackbarrier-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
